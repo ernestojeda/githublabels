297c40bda1f34fdb99704d6219057025d2e2d0b0
venkata-lakshmi-penna <55904867+venkata-lakshmi-penna@users.noreply.github.com>
2020-02-19 15:36:11 -0700

    change
    Signed-off-by: Venkata Lakshmi Dhatri Penna <venkata.lakshmi.dhatrix.penna@intel.com>

--- github-copy-labels.sh
+++ github-copy-labels.sh
@@ -5,14 +5,15 @@
 # access the source and target repositories.
 # This is how you authorize with the GitHub API.
 # https://help.github.com/en/articles/creating-a-personal-access-token-for-the-command-line
-GH_TOKEN=3c3b99d191fde5c85cdc384fd33ebaf5fcf3697c
+GH_TOKEN=$1
 
 # The source repository whose labels to copy.
-SRC_GH_USER=venkata-lakshmi-penna
-SRC_GH_REPO=Test-labels
+SRC_GH_USER=$2
+SRC_GH_REPO=$3
 # The target repository to add or update labels.
-TGT_GH_USER=venkata-lakshmi-penna
-TGT_GH_REPO=Project-2
+TGT_GH_USER=$4
+TGT_GH_REPO=$5
+
 
 # ---------------------------------------------------------
 
@@ -20,24 +21,21 @@ TGT_GH_REPO=Project-2
 GH_ACCEPT_HEADER="Accept: application/vnd.github.symmetra-preview+json"
 GH_AUTH_HEADER="Authorization: Bearer $GH_TOKEN"
 
-# Bash for-loop over JSON array with jq
+# Bash for-loop over JSON array with jq	
 # https://starkandwayne.com/blog/bash-for-loop-over-json-array-using-jq/
-sourceLabelsJson64=$(curl --silent -H "$GH_ACCEPT_HEADER" -H "$GH_AUTH_HEADER" https://api.github.com/repos/${SRC_GH_USER}/${SRC_GH_REPO}/labels | jq '[ .[] | { "name": .name, "color": .color, "description": .description } ]' | jq -r '.$
+sourceLabelsJson64=$(curl --silent -H "$GH_ACCEPT_HEADER" -H "$GH_AUTH_HEADER" https://api.github.com/repos/${SRC_GH_USER}/${SRC_GH_REPO}/labels | jq '[ .[] | { "name": .name, "color": .color, "description": .description } ]' | jq -r '.[] | @base64' )
 
 # for each label from source repo,
 # invoke github api to create or update
 # the label in the target repo
-
-#echo "curl --silent -X POST -d @- -H "$GH_ACCEPT_HEADER" -H "$GH_AUTH_HEADER" https://api.github.com/repos/${TGT_GH_USER}/${TGT_GH_REPO}/labels"
 for sourceLabelJson64 in $sourceLabelsJson64; do
 
     # base64 decode the json
     sourceLabelJson=$(echo ${sourceLabelJson64} | base64 --decode | jq -r '.')
-#    echo "sourceLabelJson: $sourceLabelJson"
+
     # try to create the label
     # POST /repos/:owner/:repo/labels { name, color, description }
-    # https://developer.github.com/v3/issues/labels/#create-a-label
-
+    # https://developer.github.com/v3/issues/labels/#create-a-label	
     createLabelResponse=$(echo $sourceLabelJson | curl --silent -X POST -d @- -H "$GH_ACCEPT_HEADER" -H "$GH_AUTH_HEADER" https://api.github.com/repos/${TGT_GH_USER}/${TGT_GH_REPO}/labels)
 
     # if creation failed then the response doesn't include an id and jq returns 'null'
@@ -46,11 +44,10 @@ for sourceLabelJson64 in $sourceLabelsJson64; do
     # if label wasn't created maybe it's because it already exists, try to update it
     if [ "$createdLabelId" == "null" ]
     then
-        updateLabelResponse=$(echo $sourceLabelJson | curl --silent -X PATCH -d @- -H "$GH_ACCEPT_HEADER" -H "$GH_AUTH_HEADER" https://api.github.com/repos/${TGT_GH_USER}/${TGT_GH_REPO}/labels/$(echo $sourceLabelJson | jq -r '.name | @u$
-        echo "Update label response:\n"$updateLabelResponse"\n"
+       updateLabelResponse=$(echo $sourceLabelJson | curl --silent -X PATCH -d @- -H "$GH_ACCEPT_HEADER" -H "$GH_AUTH_HEADER" https://api.github.com/repos/${TGT_GH_USER}/${TGT_GH_REPO}/labels/$(echo $sourceLabelJson | jq -r '.name | @uri'))
+       echo "Update label response:\n"$updateLabelResponse"\n"
     else
-        echo "Create label response:\n"$createLabelResponse"\n"
+       echo "Create label response:\n"$createLabelResponse"\n"
     fi
 
 done
-
